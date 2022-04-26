5fb27a50916018d2b98b6c8197560989a8b91714
Cosmin Iordache <cosmin2iordache@gmail.com>
2022-04-26 01:58:47 +0100

    hello

---
+++ bin/dart_one_future.dart
@@ -0,0 +1,5 @@
+import 'package:dart_one_future/dart_one_future.dart' as dart_one_future;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_one_future.calculate()}!');
+}
---
+++ test/dart_one_future_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_one_future/dart_one_future.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
